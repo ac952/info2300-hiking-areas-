7a051a69000a259487a58c0c398a9f188396fd8a
Aileen <ac952@cornell.edu>
2018-02-19 23:17:01 -0500

    trials

--- index.php
+++ index.php
@@ -7,18 +7,17 @@
 
     $current_page_id = "index";
   ?>
-
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="stylesheet" type="text/css" href="style/all.css" media = "all"/>
-<!-- add javascript tag here -->
   <title>Project 1</title>
 </head>
 
 <body>
   <?php include("includes/header.php");?>
   <h1 class="titleHeading">WELCOME TO ITHACA</h1>
-  <!-- create an array of the photos in the slideshow -->
+  <!-- https://www.tripadvisor.com/Attraction_Review-g47958-d4909594-Reviews-Ithaca_Waterfalls-Ithaca_Finger_Lakes_New_York.html -->
+  <img id="waterfall" alt="waterfall" src="images/waterfall.png"/>
   <?php include("includes/footer.php"); ?>
 </body>
 </html>
--- trails.php
+++ trails.php
@@ -7,11 +7,6 @@
 
     $current_page_id = "trails";
   ?>
-<!--
-  assign array of elements to a variable called $info
-  make a function called print_info with $an_info as a parameter.
-
-  echo a list element of $an_info -->
 
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -22,9 +17,45 @@
 <body>
   <?php include("includes/header.php");?>
   <h1 class="titleHeading">POPULAR TRAILS</h1>
-  <!-- In the body:
-  create a foreach loop instead of repeating codes.
-  In the for each loop, set the variable of the array ($info) to a key called $i, then print the key $i -->
-  <?php include("includes/footer.php"); ?>
+  <div id="trailspace">
+<div id="trail1">
+
+      <img id="img1" src="images/one.png">
+      <div id="text1">
+      <p>
+      Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry. Lorem Ipsum has been the industry's
+         standard dummy text ever since the 1500s, when an unknown
+         printer took a galley of type and scrambled it to make a
+         type specimen book. It has survived not only five centuries,
+         but also the leap into electronic typesetting, remaining essentially
+         unchanged. It was popularised in the 1960s with the release of
+          Letraset sheets containing Lorem Ipsum passages, and more
+          recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+      </p>
+    </div>
+    </div>
+
+    <div id="trail2">
+
+      <img id="img2" src="images/one.png">
+      <div id="text2">
+      <p>
+      Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry. Lorem Ipsum has been the industry's
+         standard dummy text ever since the 1500s, when an unknown
+         printer took a galley of type and scrambled it to make a
+         type specimen book. It has survived not only five centuries,
+         but also the leap into electronic typesetting, remaining essentially
+         unchanged. It was popularised in the 1960s with the release of
+          Letraset sheets containing Lorem Ipsum passages, and more
+          recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+      </p>
+    </div>
+    </div>
+  </div>
+
 </body>
 </html>
